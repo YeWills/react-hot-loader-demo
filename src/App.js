2f6b0b14c82d968a2b78b8a7e5b210fc1eafd6b7
hz-qq2 <601661706@qq.com>
2019-07-06 10:05:07 +0800

    react-hot-loader换成hot(module)()方式后，修复rebuild的Warning: You cannot change <Router history>

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { hot } from 'react-hot-loader';
 import { ConnectedRouter } from 'react-router-redux';
-import {createBrowserHistory as createHistory} from 'history';
 import RouterConfig from './Router'; // 路由配置
 import store from './Store'; // 引入Store
+import apphistory from './history'; // 引入Store
 
-const history = createHistory();
 
 
 const App = ()=>{
   return (
       <Provider store={store}>
-        <ConnectedRouter history={history}>
+        <ConnectedRouter history={apphistory}>
           <RouterConfig />
         </ConnectedRouter>
       </Provider>
